58503cac26a265368e3a926b06bba218fc468fea
iidiong <idong30@hotmail.com>
2020-03-07 07:07:11 -0500

    Feature: Added the following feature 1. scrollTo 2. Scroll-snap-type 3. updated readMe

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # Landing Page Project
 
+Landing page project uses javaScript to create an interactive navigation experience for users.
+This project build on the starter project with initial instructions below.
+
 ## Table of Contents
 
-* [Instructions](#instructions)
+- [Instructions](#instructions)
+
+- [How it works](#howItworks)
 
 ## Instructions
 
@@ -11,3 +16,13 @@ The starter project has some HTML and CSS styling to display a static version of
 To get started, open `js/app.js` and start building out the app's functionality
 
 For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+
+## How_it_works
+
+When there is no section(s) on the page, no navigation bar is displayed
+
+When new sections are added to the page, the navigation bar is displayed, and navigation items for each sections are dynamically added to page
+
+When navigation item on the nav bar is clicked, the page scrolls smoothly to the selected section
+
+When user pan or mousewheel scroll through the sections, scroll postion is enforced to display one section at time.
--- js/app.js
+++ js/app.js
@@ -1,57 +1,95 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
-*/
+ *
+ */
 
 /**
  * Define Global Variables
- * 
-*/
-
+ *
+ */
+let navbarMenu;
+let navbarList;
+let navSection;
+let activeSection;
+let navListItem;
 
 /**
  * End Global Variables
  * Start Helper Functions
- * 
-*/
-
-
+ *
+ */
+navSection = document.querySelectorAll("[id^='section']");
 
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
-*/
+ *
+ */
 
 // build the nav
-
+navbarMenu = document.querySelector(".navbar__menu");
+navbarList = document.querySelector("#navbar__list");
 
 // Add class 'active' to section when near top of viewport
-
+let active = document.querySelectorAll("section");
+if (active !== 0) {
+  for (let i = 0; i < active.length; i++) {
+    active[i].classList.add("active");
+  }
+}
 
 // Scroll to anchor ID using scrollTO event
-
+if (navSection !== 0) {
+  function scrollToSection(section) {
+    document.querySelector(section).scrollTo();
+  }
+}
 
 /**
  * End Main Functions
  * Begin Events
- * 
-*/
+ *
+ */
 
-// Build menu 
-
+// Build menu
 // Scroll to section on link click
+if (navSection !== 0) {
+  for (let k = 0; k < navSection.length; k++) {
+    navListItem = document.createElement("li");
+    activeSection = navSection[k].getAttribute("id");
+    navListItem.innerHTML = `<a href="#${activeSection}">${navSection[
+      k
+    ].getAttribute("data-nav")}</a>`;
+    navbarList.appendChild(navListItem);
+    navListItem.className = "list-item";
+  }
+} else {
+  navbarList.remove();
+}
 
 // Set sections as active
+navbarList.addEventListener("click", setActive, false);
 
+function setActive(event) {
+  for (let i = 0; i < navSection.length; i++) {
+    navSection[i].classList.remove("your-active-class");
+  }
 
+  if (event.target && event.target.matches("li a")) {
+    let sectDataNavAttribute = `[data-nav='${event.target.textContent}']`;
+    scrollToSection(sectDataNavAttribute);
+    document
+      .querySelector(sectDataNavAttribute)
+      .classList.toggle("your-active-class");
+  }
+}
